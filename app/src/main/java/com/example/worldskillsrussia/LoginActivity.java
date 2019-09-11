ba384714d0e909b434412be97ce6ec283742e64e
ElementiumST <starkov123123@gmail.com>
2019-09-11 11:52:22 +0300

    Фиксим баги

--- app/src/main/java/com/example/worldskillsrussia/LoginActivity.java
+++ app/src/main/java/com/example/worldskillsrussia/LoginActivity.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.worldskillsrussia.ui.utils.DataBaseUtils;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -18,11 +20,11 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText email;
     private EditText pass;
+    DataBaseUtils dbu = new DataBaseUtils(this);
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        if(autoLogin()) finish();
         email = findViewById(R.id.email_lable);
         pass = findViewById(R.id.pass_label);
         Button b = findViewById(R.id.login_in_button);
@@ -30,8 +32,8 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(!email.getText().toString().equals("") || pass.getText().toString().length() > 5){
-                    if(hasInDatabase(email.getText().toString(), pass.getText().toString())) {
-                        MainActivity.setlogin(email.getText().toString(), pass.getText().toString());
+                    if(dbu.hasInDatabase(email.getText().toString(), pass.getText().toString())) {
+                        setlogin(email.getText().toString(), pass.getText().toString());
 
                         finish();
                     } else
@@ -49,10 +51,7 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
-    private boolean hasInDatabase(String login, String pass) {
-        return true;
-        //TODO Связь с датабазой
-    }
+
     private void save(String login, String pass){
         File f = new File("app//data//login.txt");
         if(!f.exists())
@@ -65,33 +64,6 @@ public class LoginActivity extends AppCompatActivity {
         } else{ Toast.makeText(getApplicationContext(), "I cant write login data", Toast.LENGTH_SHORT);}
 
     }
-    public boolean autoLogin() {
-        File f = new File("app//data//login.txt");
-        if(!f.exists())
-            try {f.createNewFile(); return false;} catch (Exception e) {Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT);}
-        if(f.canRead()) {
-            try(FileReader fr = new FileReader(f)) {
-                String login= "", pass = "";
-                String bufer = "";
-                int i;
-                while((i = fr.read()) != -1) {
-                    if("(".equals((char) i)) {
-                        login = bufer;
-                        bufer = "";
-                    } else if(")".equals((char) i)) {
-                        pass = bufer;
-                        break;
-                    }else {
-                        bufer += (char) i;
-                    }
-                }
-                if(hasInDatabase(login, pass)) {MainActivity.setlogin(login, pass); return true;}
-                else return false;
-            } catch (Exception e)  {Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT);}
 
-        } else{ Toast.makeText(getApplicationContext(), "I cant read login data", Toast.LENGTH_SHORT);}
-
-        return  false;
-    }
 
 }
--- app/src/main/java/com/example/worldskillsrussia/MainActivity.java
+++ app/src/main/java/com/example/worldskillsrussia/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.worldskillsrussia;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.drawable.Animatable;
 import android.os.Bundle;
 
 import com.example.worldskillsrussia.ui.home.Post;
 
+import com.example.worldskillsrussia.ui.utils.DataBaseUtils;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -30,19 +32,24 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileReader;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private AppBarConfiguration mAppBarConfiguration;
 
     private static boolean isAuth = false;
     private static String email;
+    private static String name;
     private static String password;
     private boolean firstLaunch = false;
+    DataBaseUtils dbu = new DataBaseUtils(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(!LoginActivity.autoLogin())
+        if(!dbu.autoLogin())
         {
             Toast.makeText(getApplicationContext(), "Вы не авторизованны! Контент ограничен.", Toast.LENGTH_LONG).show();
         }
@@ -66,6 +73,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        if(email != null) {TextView maill = navigationView.findViewById(R.id.email_header_lable); maill.setText(email);}
+        if(name != null) {TextView passl = navigationView.findViewById(R.id.username_header_lable); passl.setText(password);}
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -79,12 +89,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    @Override
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
         return true;
     }
+
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -96,31 +108,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onResume() {
         super.onResume();
-        if(!isAuth()) return;
-        Object[] arr = getUserData(email, password);
-        TextView etv = findViewById(R.id.email_header_lable);
-        TextView utv = findViewById(R.id.username_header_lable);
-        etv.setText((String)arr[0]);
-        utv.setText((String)arr[1]);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        if(email != null) {TextView maill = navigationView.findViewById(R.id.email_header_lable); maill.setText(email);}
+        if(name != null) {TextView passl = navigationView.findViewById(R.id.username_header_lable); passl.setText(password);}
 
 
     }
-    public static boolean isAuth() {
+
+    public boolean isAuth() {
         return isAuth;
     }
-    public static void setlogin(String nemail, String npassword) {
+    public void setlogin(String nemail, String nname, String npassword) {
         isAuth = true;
+        name = nname;
         email = nemail;
         password = npassword;
 
+
     }
-    private Object[] getUserData(String email, String pass) {
-        Object[] arr = new Object[3];
-        arr[1] = "username";
-        if(email.equals("starkov123123@gmail.com")) arr[1] = "stark";
-        arr[0] = email; arr[2] = pass;
-        return arr;
-    }
+
 
     public void onClick(View view) {
         if(view.getId() == R.id.user_image && !isAuth()){
@@ -128,5 +134,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(inf);
         }
     }
+
 
 }
